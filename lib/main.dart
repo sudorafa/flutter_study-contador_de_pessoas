031c213b426a18694a4961d0c6533459ca1682d5
Rafael Eduardo Lima <rafael.santos@midway.com.br>
2020-08-23 16:18:56 -0300

    Create APP

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      title: "Contador de Pessoas",
+      home: Container(
+        color: Colors.white,
+      )));
+}
