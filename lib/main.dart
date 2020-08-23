fb3a2ab0a45693ff6debd77d0a6b80f69b301a71
Rafael Eduardo Lima <rafael.santos@midway.com.br>
2020-08-23 16:45:41 -0300

    coll, text, button, row and hierarc

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,32 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
       title: "Contador de Pessoas",
-      home: Container(
-        color: Colors.white,
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "Pessoas: 0",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FlatButton(
+                child: Text("+1",
+                    style: TextStyle(fontSize: 40.0, color: Colors.white)),
+                onPressed: () {},
+              ),
+              FlatButton(
+                child: Text("-1",
+                    style: TextStyle(fontSize: 40.0, color: Colors.white)),
+                onPressed: () {},
+              ),
+            ],
+          ),
+          Text(
+            "Pode Entrar",
+            style: TextStyle(color: Colors.white, fontSize: 30.0),
+          )
+        ],
       )));
 }
